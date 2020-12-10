f0eba6ebf12d41cc48798cc6a817b3f14293e09c
Sonesh Khatry <sonesh.aps@gmail.com>
2020-12-10 13:20:10 +0545

    Sound Delay Fixed

--- src/components/launchpadBody.jsx
+++ src/components/launchpadBody.jsx
@@ -49,13 +49,24 @@ const rowFour = [
 
 
 class LaunchpadBody extends Component {
+    SoundPlayLoading =() => {
+
+        const soundOne = new Howl({
+            src:['1.wav','2.wav','3.wav','4.wav','21.wav','22.wav','23.wav','24.wav','31.wav','32.wav','33.wav','34.wav','41.wav','42.wav','43.wav','44.wav']
+        });
+        soundOne.mute(true);
+        soundOne.play();
+        soundOne.mute(false);
+    }
+    componentDidMount() {
+        this.SoundPlayLoading();
+      }
+    
     SoundPlay =(src) => {
         const sound = new Howl({
             html5 : true ,
             preload : true,
             src
-            
-
         });
         sound.play();
         Howler.volume(0.5)
@@ -89,7 +100,7 @@ class LaunchpadBody extends Component {
         })
     }
     render() { 
-        return ( 
+        return (   
          <div className="launchpad-body flex-grow-1 d-flex flex-column"> 
             <div className="d-flex"> 
                 {this.RenderFirstRow()}
