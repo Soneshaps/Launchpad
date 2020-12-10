ad432255c43762fa1884ba0e2ab27d6f545db8a9
Sonesh Khatry <sonesh.aps@gmail.com>
2020-12-10 22:53:43 +0545

    Double sound in touch devices fixed

--- src/components/launchpadBody.jsx
+++ src/components/launchpadBody.jsx
@@ -46,6 +46,10 @@ const rowFour = [
     {sound : FourthFour}
 ]
 
+if (typeof document !== 'undefined') {
+    var isTouch = 'ontouchstart' in document.documentElement;
+ }
+
 
 
 class LaunchpadBody extends Component {   
@@ -60,28 +64,28 @@ class LaunchpadBody extends Component {
     RenderFirstRow=()=>{
         return rowOne.map((soundObj, index) =>{
             return(
-                <div key={index} className="button buttonOne" onTouchStart={(e)=> {this.SoundPlay(soundObj.sound);e.preventDefault(); }} onClick={()=> this.SoundPlay(soundObj.sound)}></div> 
+                <div key={index} className="button buttonOne" onTouchStart={()=> this.SoundPlay(soundObj.sound)} onClick={isTouch ? undefined:()=> this.SoundPlay(soundObj.sound)}></div> 
             )
         })
     }
     RenderSecondRow=()=>{
         return rowTwo.map((soundObj, index) =>{
             return(
-                <div key={index} className="button buttonTwo" onTouchStart={(e)=> {this.SoundPlay(soundObj.sound);e.preventDefault(); }} onClick={()=> this.SoundPlay(soundObj.sound)}></div> 
+                <div key={index} className="button buttonTwo" onTouchStart={()=> this.SoundPlay(soundObj.sound)} onClick={isTouch ? undefined:()=> this.SoundPlay(soundObj.sound)}></div> 
             )
         })
     }
     RenderThirdRow=()=>{
         return rowThree.map((soundObj, index) =>{
             return(
-                <div key={index} className="button buttonThree" onTouchStart={(e)=> {this.SoundPlay(soundObj.sound);e.preventDefault(); }} onClick={()=> this.SoundPlay(soundObj.sound)}></div> 
+                <div key={index} className="button buttonThree" onTouchStart={()=> this.SoundPlay(soundObj.sound)} onClick={isTouch ? undefined:()=> this.SoundPlay(soundObj.sound)}></div> 
             )
         })
     }
     RenderFourthRow=()=>{
         return rowFour.map((soundObj, index) =>{
             return(
-                <div key={index} className="button buttonFour" onTouchStart={(e)=> {this.SoundPlay(soundObj.sound);e.preventDefault(); }} onClick={()=> this.SoundPlay(soundObj.sound)}></div> 
+                <div key={index} className="button buttonFour" onTouchStart={()=> this.SoundPlay(soundObj.sound)} onClick={isTouch ? undefined:()=> this.SoundPlay(soundObj.sound)}></div> 
             )
         })
     }
